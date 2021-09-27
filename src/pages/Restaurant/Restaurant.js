5eef47e8ebfb77d6bbe1e156c6ceba0866f49911
Alexandre Repaire-Carlier <repaire.alexandre@gmail.com>
2021-09-27 11:05:09 +0200

    delete route fichier restaurant

--- src/pages/Restaurant/Restaurant.js
+++ src/pages/Restaurant/Restaurant.js
@@ -6,12 +6,7 @@ const Restaurant = () => {
     return (
         <Fragment>
             <h1>Liste des Restaurants cherchés</h1>
-            <Route path="/restaurant" exact>
-                <Link to={'/restaurant/comment'}>Load comment</Link>
-            </Route>
-            <Route path={`/restaurant/comment`}>
-                <h1>Mon component comment resto</h1>
-            </Route>
+
         </Fragment>
     );
 };
